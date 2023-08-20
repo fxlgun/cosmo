5a95670a9446e5877021d0edafe9cdca893ab319
Falgun <77203052+fxlgun@users.noreply.github.com>
2023-08-20 19:14:24 +0530

    fixed folder structure

--- server/index.js
+++ server/index.js
@@ -21,8 +21,11 @@ mongoose.connect(process.env.MONGOURL, {
   });
 
 // ... Define your routes and app logic ...
+app.get("/", (req, res) => {
+  res.send("hi")
+})
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 7000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
