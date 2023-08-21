bf1f4f9fcef4759d4f116fa087f1e96e14492384
krishna <krishnashewaramani1527@gmail.com>
2023-08-21 20:57:11 +0530

    login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
-
-import './App.css';
+import "./App.css";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <div className="App">
-      hi
+      <Login />
     </div>
   );
 }
